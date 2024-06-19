29ee4a8101eff67054d6cc7594c0d2eba71f5128
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-06-19 23:51:12 +0200

    Add files via upload

---
+++ ConcatAndJoin/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConcatAndJoin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            string[] strArray = { "abecadlo", "banan", "cebula", "dynia", "ekran", "flet" };
+
+            string numString = string.Concat(numArray);
+            Console.WriteLine(numString);
+
+            string strString = string.Join(", ", strArray);
+            Console.WriteLine(strString);
+        }
+    }
+}
